08fbbbd4cf13af6e87e6c1bbd7a030cab06ab892
Jan Korf <jankorf91@gmail.com>
2019-03-25 11:22:24 +0100

    Made market data decimals nullable (new markets dont have data)

--- Huobi.Net/Objects/HuobiMarketData.cs
+++ Huobi.Net/Objects/HuobiMarketData.cs
@@ -9,33 +9,33 @@ namespace Huobi.Net.Objects
         /// <summary>
         /// The highest price
         /// </summary>
-        public decimal High { get; set; }
+        public decimal? High { get; set; }
         /// <summary>
         /// The lowest price
         /// </summary>
-        public decimal Low { get; set; }
+        public decimal? Low { get; set; }
         /// <summary>
         /// The price at the opening
         /// </summary>
-        public decimal Open { get; set; }
+        public decimal? Open { get; set; }
         /// <summary>
         /// The last price
         /// </summary>
-        public decimal Close { get; set; }
+        public decimal? Close { get; set; }
         /// <summary>
         /// The amount of the symbol trades
         /// </summary>
-        public decimal Amount { get; set; }
+        public decimal? Amount { get; set; }
         /// <summary>
         /// The volume of the symbol trades (amount * price)
         /// </summary>
         [JsonProperty("vol")]
-        public decimal Volume { get; set; }
+        public decimal? Volume { get; set; }
         /// <summary>
         /// The number of trades
         /// </summary>
         [JsonProperty("count")]
-        public int TradeCount { get; set; }
+        public int? TradeCount { get; set; }
     }
 
     public class HuobiMarketKline : HuobiMarketData
