c01b310ca2147309799f9a8c57b3791436611ed0
Jkorf <jankorf91@gmail.com>
2021-11-16 11:39:05 +0100

    Added test for all methods available in interface

--- Huobi.Net.UnitTests/HuobiClientTests.cs
+++ Huobi.Net.UnitTests/HuobiClientTests.cs
@@ -9,6 +9,11 @@ using System.Net.Http;
 using CryptoExchange.Net.Authentication;
 using System.Threading.Tasks;
 using Huobi.Net.Enums;
+using System.Reflection;
+using Huobi.Net.Client.Rest.Spot;
+using System.Diagnostics;
+using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.Sockets;
 
 namespace Huobi.Net.UnitTests
 {
@@ -82,6 +87,47 @@ namespace Huobi.Net.UnitTests
                 Assert.DoesNotThrow(() => symbol.ValidateHuobiSymbol());
             else
                 Assert.Throws(typeof(ArgumentException), () => symbol.ValidateHuobiSymbol());
+        }
+
+        [Test]
+        public void CheckRestInterfaces()
+        {
+            var assembly = Assembly.GetAssembly(typeof(HuobiClientSpot));
+            var ignore = new string[] { "IHuobiClientSpot" };
+            var clientInterfaces = assembly.GetTypes().Where(t => t.Name.StartsWith("IHuobiClientSpot") && !ignore.Contains(t.Name));
+
+            foreach (var clientInterface in clientInterfaces)
+            {
+                var implementation = assembly.GetTypes().Single(t => t.IsAssignableTo(clientInterface) && t != clientInterface);
+                int methods = 0;
+                foreach (var method in implementation.GetMethods().Where(m => m.ReturnType.IsAssignableTo(typeof(Task))))
+                {
+                    var interfaceMethod = clientInterface.GetMethod(method.Name, method.GetParameters().Select(p => p.ParameterType).ToArray());
+                    Assert.NotNull(interfaceMethod);
+                    methods++;
+                }
+                Debug.WriteLine($"{clientInterface.Name} {methods} methods validated");
+            }
+        }
+
+        [Test]
+        public void CheckSocketInterfaces()
+        {
+            var assembly = Assembly.GetAssembly(typeof(HuobiSocketClientSpot));
+            var clientInterfaces = assembly.GetTypes().Where(t => t.Name.StartsWith("IHuobiSocketClientSpot"));
+
+            foreach (var clientInterface in clientInterfaces)
+            {
+                var implementation = assembly.GetTypes().Single(t => t.IsAssignableTo(clientInterface) && t != clientInterface);
+                int methods = 0;
+                foreach (var method in implementation.GetMethods().Where(m => m.ReturnType.IsAssignableTo(typeof(Task<CallResult<UpdateSubscription>>))))
+                {
+                    var interfaceMethod = clientInterface.GetMethod(method.Name, method.GetParameters().Select(p => p.ParameterType).ToArray());
+                    Assert.NotNull(interfaceMethod);
+                    methods++;
+                }
+                Debug.WriteLine($"{clientInterface.Name} {methods} methods validated");
+            }
         }
     }
 }
