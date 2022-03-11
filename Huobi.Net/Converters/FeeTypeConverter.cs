d76eba9583b3da75d89e8a834d776bb58662e448
George Flourentzos <georgeflourentzos@gmail.com>
2022-03-11 12:16:23 +0000

    Add converters for enums related to getting withdraw fee information

---
+++ Huobi.Net/Converters/FeeTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CryptoExchange.Net.Converters;
+using Huobi.Net.Enums;
+
+namespace Huobi.Net.Converters
+{
+    internal class FeeTypeConverter : BaseConverter<FeeType>
+    {
+        public FeeTypeConverter() : this(true) { }
+        public FeeTypeConverter(bool quotes) : base(quotes) { }
+
+        protected override List<KeyValuePair<FeeType, string>> Mapping => new List<KeyValuePair<FeeType, string>>
+        {
+            new KeyValuePair<FeeType, string>(FeeType.Fixed, "fixed"),
+            new KeyValuePair<FeeType, string>(FeeType.Circulated, "circulated"),
+            new KeyValuePair<FeeType, string>(FeeType.Ratio, "ratio")
+            
+        };
+    }
+}
