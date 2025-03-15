46ae00bc465557cdb3397f27dced530869cdc0ef
nils2525 <nilslehb@gmail.com>
2025-03-15 13:57:30 +0100

    Fixed shared client balance subscription handling

--- HTX.Net/Clients/SpotApi/HTXSocketClientSpotApiShared.cs
+++ HTX.Net/Clients/SpotApi/HTXSocketClientSpotApiShared.cs
@@ -1,8 +1,8 @@
-﻿using HTX.Net.Interfaces.Clients.SpotApi;
+﻿using CryptoExchange.Net.Objects.Sockets;
 using CryptoExchange.Net.SharedApis;
-using CryptoExchange.Net.Objects.Sockets;
-using HTX.Net.Objects.Models.Socket;
 using HTX.Net.Enums;
+using HTX.Net.Interfaces.Clients.SpotApi;
+using HTX.Net.Objects.Models.Socket;
 
 namespace HTX.Net.Clients.SpotApi
 {
@@ -132,7 +132,7 @@ namespace HTX.Net.Clients.SpotApi
 
             var result = await SubscribeToAccountUpdatesAsync(
                 update => handler(update.AsExchangeEvent<IEnumerable<SharedBalance>>(Exchange, new[] { new SharedBalance(update.Data.Asset, update.Data.Available ?? 0, update.Data.Balance ?? update.Data.Available ?? 0) })),
-                ct: ct).ConfigureAwait(false);
+                2, ct: ct).ConfigureAwait(false);
 
             return new ExchangeResult<UpdateSubscription>(Exchange, result);
         }
