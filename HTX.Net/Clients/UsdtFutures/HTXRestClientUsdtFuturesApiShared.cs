5712622ba34e237b021874770990aa186234dcab
Jkorf <jankorf91@gmail.com>
2026-08-13 12:42:25 +0200

    Added Shared APIs Rest integration tests

--- HTX.Net/Clients/UsdtFutures/HTXRestClientUsdtFuturesApiShared.cs
+++ HTX.Net/Clients/UsdtFutures/HTXRestClientUsdtFuturesApiShared.cs
@@ -1327,7 +1327,7 @@ namespace HTX.Net.Clients.UsdtFutures
 
         #region Open Interest client
 
-        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, true);
+        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, false);
         async Task<HttpResult<SharedOpenInterest>> IOpenInterestRestClient.GetOpenInterestAsync(GetOpenInterestRequest request, CancellationToken ct)
         {
             var validationError = SharedClient.GetOpenInterestOptions.ValidateRequest(request, this);
